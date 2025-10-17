9ae86596728830b7291c6286c9cd2eafed63a562
rafaeldiorato <rafael.diorato@mhpsit.com>
2025-10-17 14:01:45 -0500

    Classe usuario

--- gestaodetarefas.py
+++ gestaodetarefas.py
@@ -16,3 +16,19 @@ def editar_tarefa(self, novo_titulo, nova_descricao, nova_data):
     self.titulo = novo_titulo
     self.descricao = nova_descricao
     self.data_de_vencimento = nova_data 
+
+# Classe Usuário
+class Usuario:
+    def __init__(self, nome_usuario, contrasena):
+        self.nome_usuario = nome_usuario
+        self.contasena = contrasena
+        self.tarefas = []
+    
+    def agregar_tarefas(self, tarefa):
+        self.tarefas.append(tarefa)
+    
+    def eliminar_tarefas(self, titulo_tarefa):
+        self.tarefas = [tarefa for tarefa in self.tarefas if tarefa.titulo != titulo_tarefa]
+
+    def obtener_tarefas(self):
+        return self.tarefas
